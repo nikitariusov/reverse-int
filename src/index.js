5da60a67049b61aed99dd0eb127b27d463b8648f
Nikita Usov <nikitariusov@gmail.com>
2023-01-15 19:03:07 +0200

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const reverseNumInArr = []
+  numInArr = n.toString().split('')
+  for (let i = numInArr.length -1; i >= 0; i--) {
+    reverseNumInArr.push(numInArr[i])
+  }
+
+  return parseInt(reverseNumInArr.join(''))
 }
